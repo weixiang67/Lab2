27e2b72c44567d012daf103c00f73e7a4615e272
weixiang <WEIXIANGX33.24@ichat.sp.edu.sg>
2025-10-26 18:07:36 +0800

    added userinput, average, minmax

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,42 @@
-print("ET0735 (Devops for AIot) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (Devops for AIot) - Lab 2 - Introduction to") #indentation matters in python
+    display_main_menu()
+    num_list = get_user_input()
+    print(calc_average_temperature(num_list))
+    print(calc_min_max_temperature(num_list))
+
+def display_main_menu():
+    print("display_main_menu")
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+
+    x = input()
+    temp_list = x
+    py_list = temp_list.split(",") #use .split to split the inputs into list
+    float_list = []
+    for num in py_list:
+        float_list.append(float(num)) #loops through the numbers in the list, append as float
+    return float_list
+
+def calc_average():
+    print("calc_average")
+
+def calc_average_temperature(num_list):
+    average = sum(num_list)/len(num_list)
+    return average
+    
+
+
+def calc_min_max_temperature(num_list):
+    minimum = min(num_list)
+    maximum = max(num_list)
+    return [minimum,maximum]
+
+if __name__ == "__main__": 
+    main() 
+    
+
+
+
+    
