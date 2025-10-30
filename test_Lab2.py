7a80cadda2da2e010c7818dfebc26736abfb14ec
weixiang <WEIXIANGX33.24@ichat.sp.edu.sg>
2025-10-30 14:58:09 +0800

    added pytest for lab 2

---
+++ test_Lab2.py
@@ -0,0 +1,16 @@
+import Lab2.Lab2 as lab2
+
+def test_find_min_max(): 
+    imp_list = [10,20,30,40,50]
+    result = lab2.calc_min_max_temperature(imp_list)
+    assert (result == [10,50])
+
+def test_calc_avg():
+    imp_list = [10, 1 , 1 ,1 , 1]
+    result = lab2.calc_average_temperature(imp_list)
+    assert (result == 2.8)
+
+def test_calc_median_temperature():
+    imp_list = [1,2,3,4,5]
+    result = lab2.calc_median_temperature(imp_list)
+    assert (result == 3)
