d3b1748587e710a33d217a7cfc50fde97429a7f7
weixiang67 <WEIXIANGX33.24@ichat.sp.edu.sg>
2025-11-23 21:03:25 +0800

    added minor changes

--- test_Lab2.py
+++ test_Lab2.py
@@ -14,3 +14,19 @@ def test_calc_median_temperature():
     imp_list = [1,2,3,4,5]
     result = lab2.calc_median_temperature(imp_list)
     assert (result == 3)
+
+def test_calc_min_max():
+    imp_list = [5,4,3,2,1]
+    result = lab2.calc_min_max_temperature(imp_list)
+    assert (result == [1,5])
+
+def test_calc_average():
+    imp_list = [10,20,30]
+    result = lab2.calc_average_temperature(imp_list)
+    assert (result == 20)
+
+def test_calc_median_temperature():
+    imp_list = [1, 2, 3, 4, 5]
+    result = lab2.calc_median_temperature(imp_list)
+    assert (result == 3)
+
